4327e110061bf0f15b32774d1b70a4ff8a6696a4
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-08-05 09:42:20 -0500

    first cimmit

---
+++ Hello_World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! What a brave new day!");
+            Console.ReadLine();
+            // this 
+
+        }
+    }
+}
