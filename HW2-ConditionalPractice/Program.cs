e62893be0bc2402523a47bd34c7c8663e8ddcf55
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-08-05 14:49:40 -0500

    HW2 C#

---
+++ HW2-ConditionalPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW2_ConditionalPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) { 
+            double dNum1, dNum2, dNum3;
+                try
+                {
+                Console.Write("Please enter the first number: ");
+                dNum1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Please enter the second number: ");
+                dNum2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Please enter the third number: ");
+                dNum3 = Convert.ToDouble(Console.ReadLine());
+
+                if(dNum1>=dNum2&&dNum1>=dNum3)
+                    {
+                        if(dNum2>=dNum3)
+                        {
+
+                        }
+                    }
+                } catch (Exception) { continue; }
+        }
+        }
+    }
+}
