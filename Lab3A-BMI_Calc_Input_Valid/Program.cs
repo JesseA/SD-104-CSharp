dfd7aeee43b440488e839a3b7908acef652e62aa
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-08-08 11:27:19 -0500

    Lab 3A sumbit

---
+++ Lab3A-BMI_Calc_Input_Valid/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lab3A_BMI_Calc_Input_Valid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dHeight=0, dWeight=0, dBMI=0;
+            
+                    Console.WriteLine("BMI Calculator");
+                    Console.WriteLine("");
+
+                    Console.Write("Please Enter Your Height in Inches: ");
+                    while(!double.TryParse(Console.ReadLine(), out dHeight))
+                    {
+                         Console.WriteLine("Invalid input. Height must be a posistive, rational number.");
+                        Console.WriteLine("");
+                        Console.Write("Please Enter Your Height in Inches: ");
+                    }
+
+                    Console.Write("Please Enter Your Weight in Pounds: ");
+                    while (!double.TryParse(Console.ReadLine(), out dWeight))
+                     {
+                        Console.WriteLine("Invalid input. Weight must be a posistive, rational number.");
+                        Console.WriteLine("");
+                        Console.Write("Please Enter Your Weight in Pounds: ");
+                    }
+
+                    dBMI = ((dWeight * 703) / (dHeight * dHeight));
+
+                    Console.WriteLine("Your BMI is " + dBMI);
+
+                    if (dBMI < 18.5)
+                    {
+                        Console.WriteLine("Your BMI is under 18.5 - Underweight!");
+                    }
+                    else if (dBMI <= 24.9)
+                    {
+
+                        Console.WriteLine("Your BMI is between 18.5 and 24.9 - Normal Weight");
+                    }
+                    else if (dBMI <= 29.9)
+                    {
+
+                        Console.WriteLine("Your BMI is between 24.5 and 29.9 - Overweight!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your BMI is over 29.9 - Obese!!");
+                    }
+                 Console.ReadLine();
+                
+           
+        }
+    }
+}
