957e38f85156dedc21a8745683bfa0fdf5660343
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-08-12 11:44:15 -0500

    8/2/2019 11:44AM

---
+++ Lab4-GuessMyNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab4_GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random myRand = new Random();
+            int hidden = -2, guess = -1;
+            String sExitAnswer;
+
+            
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("----------------------------------------------------------------------------------");
+                    Console.WriteLine("The Hidden Number Guessing Game");
+                    Console.WriteLine("----------------------------------------------------------------------------------");
+                    Console.WriteLine("");
+
+                    hidden = myRand.Next(100);
+                   // Console.WriteLine(hidden);
+
+                    do
+                    {
+                        do { Console.Write("Please guess a number between 0 - 100: "); }
+                        while (!Int32.TryParse(Console.ReadLine().Trim(), out guess));
+                        if (guess < 0)  { Console.WriteLine("Sorry, your guess " + guess + " was too low!"); Console.WriteLine(""); hidden = myRand.Next(100); }
+                        else if (guess > 100) { Console.WriteLine("Sorry, your guess " + guess + " was too high!"); Console.WriteLine(""); hidden = myRand.Next(100); }
+                    }
+                    while ((guess < 0) || (guess > 100));
+
+                    Console.WriteLine("");
+                    Console.WriteLine((guess == hidden) ? "Winner!! Your guess " + guess + " was Correct!! You are a Winner!!" : "Sorry, your guess " + guess + " was Wrong!! The hidden number was "+hidden+"!!");
+
+                    Console.Write("Would you like to play again? Enter 'Y' or 'Yes' to continue; Enter anyting else to Exit the program: ");
+                    sExitAnswer = Console.ReadLine().ToLower().Trim();
+                    if (("y".Equals(sExitAnswer)) || ("yes".Equals(sExitAnswer)))
+                    {  Console.WriteLine(""); continue; }
+                    else { Console.WriteLine(""); break; }
+                }
+                catch (Exception) { Console.WriteLine(""); continue; }
+            }
+        }
+    }
+}
---
+++ Lab7-ParallelArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab7_ParallelArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] Names = { "Rick Sanchez", "Morty Smith", "Jerry Smith", "Beth Smith", "Summer Smith", "Lupe Hurtado" };
+            String[] Phone = {"555-1334","555-3882","555-8211","555-1617","555-2803","555-8888" };
+            String strSearch;
+
+
+            Console.WriteLine("Phone Book");
+            Console.WriteLine("");
+            //prompt user
+            Console.Write("Please type a name to search the phone book: ");
+            strSearch = Console.ReadLine();
+
+            for(int n=0; n<Names.Length;n++)
+            {
+                if(Names[n].IndexOf(strSearch) > -1)
+                {
+                    Console.WriteLine(Names[n] + " -> " + Phone[n]);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
