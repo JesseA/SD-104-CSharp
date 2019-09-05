b7148c35f7c60a3326800c4a7ad7deac7b167d88
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-09-05 15:08:52 -0500

    9/5/19 #002

---
+++ MVCLab2-AddingAView/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLab1_AddingAController.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Neo...the Matrix has you...";
+        }
+
+        public IActionResult WhiteRabbit()
+        {
+            return View();
+        }
+    }
+}
---
+++ MidTermQuiz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace MidTermQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string thisMeal, mealString;
+            int mealHour = -1;
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            Console.Write("Please enter a meal: ");
+            thisMeal = Console.ReadLine().Trim().ToLower();
+
+            mealHour = TimeOfMeal(thisMeal);
+
+            switch (mealHour)
+            {
+                case 6: mealString=" is eaten at 6 A.M."; break;
+                case 9: mealString = " is eaten at 9 A.M."; break;
+                case 12: mealString = " is eaten at Noon."; break;
+                case 18: mealString = " is eaten at 6 P.M."; break;
+                default: mealString = " is eaten at Midnight."; break;
+            }
+
+            Console.WriteLine("");
+
+            thisMeal = textInfo.ToTitleCase(thisMeal);
+            Console.WriteLine(thisMeal+mealString);
+            Console.ReadLine();
+        }
+
+        public static int TimeOfMeal(string meal)
+        {
+            int hour=-1;
+
+            switch(meal)
+            {
+                case "breakfast": hour = 6; break;
+                case "brunch": hour = 9; break;
+                case "lunch": hour = 12; break;
+                case "dinner": hour = 18; break;
+                default: hour = 0; break;
+            }
+
+            return hour;
+        }
+    }
+}
