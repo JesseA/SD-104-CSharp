9d36808b610a3fe53c4bc8faa0443706cc6a314b
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-09-12 11:06:52 -0500

    Lab 6

---
+++ MVC-Lab6-LandingPageAndDeleteStories/Data/Migrations/20190912142132_Second.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicStore.Data.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Song",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFeatured",
+                table: "Song",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Song");
+
+            migrationBuilder.DropColumn(
+                name: "IsFeatured",
+                table: "Song");
+        }
+    }
+}
