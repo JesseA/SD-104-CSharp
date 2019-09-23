039aa374cf2f072b39dcf426c15936a1b318d2a8
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-09-23 14:52:23 -0500

    FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,7 +5,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 101; i++){Console.WriteLine((i%3==0&&i%5==0)?"FizzBuzz":(i%3==0)?"Fizz":(i%5==0)?"Buzz":i.ToString());}
+            for (int i = 1; i < 101; i++){Console.WriteLine((i%15==0)?"FizzBuzz":(i%3==0)?"Fizz":(i%5==0)?"Buzz":i.ToString());}
             Console.ReadLine();
         }
     }
