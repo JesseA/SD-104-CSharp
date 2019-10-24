f27f535bad4edbf7ffcdb9a643f4e650fc617cb3
Jesse Alexander Foy III <jesse.a.foy@gmail.com>
2019-10-24 08:56:06 -0500

    Project 10-24-19

---
+++ StackObject/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack ms = new MyStack();
+            ms.Push("World");
+            ms.Push(" ");
+            ms.Push("Hello");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(ms.Peek());
+                ms.Pop();
+            }
+        }
+    }
+
+    class MyStack
+    {
+        public IList<Object> myStack;
+        public MyStack()
+        {
+            myStack = new List<Object>();
+        }
+
+        public void Push(object o)
+        {
+            myStack.Add(o);
+        }
+        public void Pop()
+        {
+
+            myStack.RemoveAt(myStack.Count - 1);
+        }
+        public Object Peek()
+        {
+
+            Object o = myStack[myStack.Count-1];
+            return o;
+        }
+    }
+}
---
+++ WebBookstore_FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebBookstore_FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
